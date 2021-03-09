c0c931bfc2a5a45ef1b6dd540791dc3429006e06
Nguyễn Thị Ngọc Ánh <anhntn@Friday.local>
2021-03-09 13:15:46 +0700

    html

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Tổng hợp tài liệu</h1>
+        <ul>
+          <li>
+            <p>
+              Google Drive chứa tài liệu:&nbsp;
+              <a href="">
+                Link
+              </a>
+            </p>
+          </li>
+
+          </ul>
+      </>
+    );
+  }
+
+}
+export default App;
