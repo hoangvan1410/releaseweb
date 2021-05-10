f08c843dbeec11017f8e4037f90d9e18d744f812
hoangvan1410 <76679911+hoangvan1410@users.noreply.github.com>
2021-05-10 14:41:31 +0700

    11/5

--- src/App.js
+++ src/App.js
@@ -126,58 +126,78 @@ class App extends React.Component {
           </ul>
         <hr />
         <h1>Cập nhật quá trình</h1>
+
+        <h2>04/05/2021</h2>
+
+        <h2>27/04/2021</h2>
+
+        <h2>20/04/2021</h2>
+        <ul>
+          <li><a href="">
+          Chức năng gửi thông báo nhắc nhở người dùng đo nhịp tim </a></li>
+          <li><a href="">
+          Cập nhật chương 4 luận văn </a></li>
+        </ul>
+
+        <h2>13/04/2021</h2>
+        <ul>
+          <li><a href="">
+          Giao diện lịch sử đo nhịp tim</a></li>
+          <li><a href="">
+          Docker file cho backend</a></li>
+          <li><a href="">
+          Cập nhật chương 1 luận văn</a></li>
+        </ul>
+
+        <h2>06/04/2021</h2>
+        <ul>
+          <li><a href="">
+            Hàm xử lý nhiễu dữ liệu nhịp tim sau bước FFT trong quy trình đo nhịp tim </a></li>
+          <li><a href="">
+            Chức năng đo nhịp tim và hiển thị biểu đồ trong thời gian đo</a></li>
+          <li><a href="">
+            Chức năng hiển thị kết quả đo sau 20 giây</a></li>
+        </ul>
+
+        <h2>30/03/2021</h2>
+        <ul>
+          <li><a href="https://drive.google.com/file/d/1yC5zoMWnNz0PQp6zKzwOAg9gNtL5LL3c/view">
+              Prototype mô tả luồng hoạt động ứng dụng</a></li>
+          <li><a href="">
+              Chức năng thay đổi thông tin cá nhân của người dùng trên giao diện</a></li>
+          <li><a href="">
+              Chức năng đo nhịp tim của ứng dụng</a></li>
+        </ul>
+
+        <h2>23/03/2021</h2>
+        <ul>
+          <li><a href="https://drive.google.com/file/d/1nFsy44llvnitNOSeBL6GU8MqjfaBdaOq/view?usp=sharing">
+              Mô hình hoá mô hình lưu trữ cơ sở dữ liệu</a></li>
+          <li><a href="">
+              Tính năng đo nhịp tim (cần cải thiện thêm)</a></li>
+          <li><a href="https://drive.google.com/file/d/16vFXtpweo4ySWccbEqynNK5TIQuNWQ9K/view?usp=sharing">
+              Sơ đồ cơ sở dữ liệu</a></li>
+        </ul>
         
         <h2>16/03/2021</h2>
         <ul>
-          <li>
-            <p>
-            Cập nhật các hàm liên quan tới tính năng đo nhịp tim
-            </p>
-          </li>
-          <li>
-            <p>
-            Chức năng đăng kí/ đăng nhập/ đăng nhập bằng Google
-            </p>
-          </li>
-          <li>
-            <p>
-            Tạo dữ liệu giả cho mục đích test
-            </p>
-          </li>
-          <li>
-            <p>
-            Cập nhật Prototype hoàn chỉnh
-            </p>
-          </li>
+          <li><p>Cập nhật các hàm liên quan tới tính năng đo nhịp tim</p></li>
+          <li><p>Chức năng đăng kí/ đăng nhập/ đăng nhập bằng Google</p></li>
+          <li><p>Tạo dữ liệu giả cho mục đích test</p></li>
+          <li><a href="https://drive.google.com/file/d/1yC5zoMWnNz0PQp6zKzwOAg9gNtL5LL3c/view">
+              Prototype hoàn chỉnh </a></li>
         </ul>
         
         <h2>09/03/2021</h2>
         <ul>
-          <li>
-            <a href="https://drive.google.com/file/d/1RhfNkrwM8JZmDkMdr1ZBByoih-wD7nwN/view?usp=sharing">
-              Prototype tính năng đo nhịp tim
-            </a>
-          </li>
-          <li>
-            <a href="">
-              Design logo app
-            </a>
-          </li>
-          <li>
-            <a href="https://vsrelease072021.herokuapp.com/">
-              Website release
-            </a>
-          </li>
-          <li>
-            <a href="https://drive.google.com/file/d/16vFXtpweo4ySWccbEqynNK5TIQuNWQ9K/view?usp=sharing">
-              Sơ đồ lưu trữ dữ liệu
-            </a>
-          </li>
-          <li>
-            <p>
-              Phân tích, chạy thử thư viện / mã nguồn mở 
-            </p>
-          </li>
+          <li><a href="https://drive.google.com/file/d/1RhfNkrwM8JZmDkMdr1ZBByoih-wD7nwN/view?usp=sharing">
+              Prototype tính năng đo nhịp tim</a></li>
+          <li><a href="">Design logo app</a></li>
+          <li><a href="https://vsrelease072021.herokuapp.com/">
+              Website release</a></li>
+          <li><a href="https://drive.google.com/file/d/16vFXtpweo4ySWccbEqynNK5TIQuNWQ9K/view?usp=sharing">
+              Sơ đồ lưu trữ dữ liệu</a></li>
+          <li><p>Phân tích, chạy thử thư viện / mã nguồn mở </p></li>
         </ul>
       </>
     );
